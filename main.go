34596c002555efbab1452761b5b5d919304ff493
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2021-09-13 12:42:25 +0200

    structs

--- main.go
+++ main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+type movie struct {
+	title string
+	year int
+}
+
+func newMovie(title string, year int) *movie {
+	return &movie{title: title, year: year}
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	godfather := newMovie("The Godfather", 1972)
+	fmt.Println(godfather)
+
+	pulpFiction := movie{title: "Pulp Fiction", year: 1994}
+	fmt.Println(pulpFiction)
+
+	fightClub := movie{title: "Fight Club"}
+	fmt.Println(fightClub)
+
+	fightClub.year = 1999
+	fmt.Println(fightClub)
 }

6308bd0e8e576a8d8e4be422c89faa6f2728c8f1
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2021-09-13 11:50:41 +0200

    variables

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	var name1 string
+	name1 = "Andrzej"
+
+	name2 := "Alfred"
+
+	name3 := getName3()
+
+	fmt.Printf("Hello %s!\n", name1)
+	fmt.Printf("Hello %s!\n", name2)
+	fmt.Printf("Hello %s!\n", name3)
+}
+
+func getName3() string {
+	return "Antoni"
 }

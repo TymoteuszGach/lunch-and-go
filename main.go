c4cba1f64d038907a3df9911c955d3a6b0594a9e
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2021-09-13 12:29:21 +0200

    pointers

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	i := 1
+	var j *int
+	j = &i
+	fmt.Printf("i: %d\n", i)
+	fmt.Printf("j: %d\n", *j)
+
+	zeroval(i)
+	fmt.Printf("zeroval: %d\n", i)
+
+	zeroptr(&i)
+	fmt.Printf("zeroptr: %d\n", i)
+}
+
+func zeroval(val int) {
+	val = 0
+}
+
+func zeroptr(ptr *int) {
+	*ptr = 0
 }

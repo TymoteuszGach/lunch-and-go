4782280d05d8fb0c78bc6d469943b11b2d3694a9
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2021-09-13 12:01:54 +0200

    functions

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	result1 := sum(3, 7)
+	a, b := getValues()
+	c, err := tryCalculate()
+
+	fmt.Printf("sum: %d\n", result1)
+	fmt.Printf("a: %d, b: %d\n", a, b)
+	fmt.Printf("c: %d, err: %v\n", c, err)
+}
+
+func sum(a, b int) int {
+	return a + b
+}
+
+func getValues() (int, int) {
+	return 5, 8
+}
+
+func tryCalculate() (int, error) {
+	if time.Now().Weekday() == time.Friday {
+		return 0, fmt.Errorf("chill out")
+	}
+	return 10, nil
 }

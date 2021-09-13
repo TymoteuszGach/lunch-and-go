6509067d132066d4889411036da4b38fd166dbe2
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2021-09-13 14:46:44 +0200

    channels

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func sum(n int, result chan<- int){
+	sum := 0
+	for i:= 1; i <= n; i++{
+		sum += i
+	}
+
+	time.Sleep(1*time.Second)
+
+	result <- sum
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	resultChan := make(chan int)
+	go sum(10, resultChan)
+
+	doSomethingElse()
+
+	select{
+	case result := <-resultChan:
+		fmt.Printf("result is: %d\n", result)
+	case <-time.After(2*time.Second):
+		fmt.Println("timeout!!11")
+	}
+}
+
+func doSomethingElse() {
+	fmt.Println("doing something else...")
 }
